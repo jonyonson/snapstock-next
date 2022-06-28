c1dbdb973428b024d0b4bb538d24ef2103915560
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2022-06-28 05:36:36 -0500

    test: add test for widget

--- components/__tests__/Widget.test.tsx
+++ components/__tests__/Widget.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Widget from '../Widget';
 
 describe('Widget', () => {
-  it('renders a widget', () => {
+  it('renders a widget with name and correctly formatted price', () => {
     render(
       <Widget
         name={'DJIA'}
@@ -16,6 +16,23 @@ describe('Widget', () => {
 
     const widget = screen.getByTestId('widget');
 
+    expect(widget).toHaveTextContent('DJIA');
     expect(widget).toHaveTextContent('31,500.58');
+  });
+
+  it('renders a widget with a correctly formatted change', () => {
+    render(
+      <Widget
+        name={'DJIA'}
+        change={823.28345}
+        price={31500.57867}
+        percentChange={2.6788}
+        loading={false}
+      />,
+    );
+
+    const widget = screen.getByTestId('widget');
+
+    expect(widget).toHaveTextContent('+823.23');
   });
 });
