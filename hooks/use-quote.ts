c21443f6f960de5d2940f07aa67ee289e0d303a3
Jonathan Taylor <jonathan.taylor.dev@gmail.com>
2022-07-02 08:28:27 -0500

    fix: typescript erorr

--- hooks/use-quote.ts
+++ hooks/use-quote.ts
@@ -4,19 +4,13 @@ import useSWR from 'swr';
 // Constants
 import { API } from 'config/constants';
 
-interface Quote extends IEXCloudQuote, YahooFinanceQuote {}
-
-interface Data {
-  quote: Quote;
-  company: Company;
-  stats: Stats;
-}
-
 function useQuote(symbol: string, refreshInterval = 0) {
   const url = `${API.QUOTE}/${symbol}`;
-  const { data, error } = useSWR<Data>(symbol ? url : null, fetch, {
-    refreshInterval,
-  });
+  const { data, error } = useSWR<YahooFinanceQuote>(
+    symbol ? url : null,
+    fetch,
+    { refreshInterval },
+  );
 
   return {
     data,
